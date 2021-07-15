95196de4cb65bf75d4afaa1e6c844922f7eaef1a
Roberto R Jr <rrjr87@outlook.com>
2021-07-15 12:48:42 -0300

    added task 1

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,15 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if (!newTaskTitle) return;
+
+    const newTask: Task = {
+      id:  Math.floor(Math.random() * 10000),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
